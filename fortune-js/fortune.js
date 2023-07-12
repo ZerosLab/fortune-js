13e936ce6068fc83d812f2e0f73e44d1e1b6d7a4
Zero <121123841+ZerosLab@users.noreply.github.com>
2023-07-12 01:42:00 -0400

    Update fortune.js
    minimalism

--- fortune-js/fortune.js
+++ fortune-js/fortune.js
@@ -1,11 +1,5 @@
 quotepath = "friends.json";
-
 elementid = "fortune";
-
-
 $.getJSON(quotepath, function(test) {
-	num = test.length;
-	rand = Math.floor(Math.random() * num);
-	fort = test[rand];
-	document.getElementById(elementid).innerHTML = fort;
+	document.getElementById(elementid).innerHTML = test[Math.floor(Math.random() * test.length)];
 });
