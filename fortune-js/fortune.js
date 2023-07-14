38bf856141f53464a87f243fa7ccbc6d0557f440
Zero <121123841+ZerosLab@users.noreply.github.com>
2023-07-14 17:33:50 -0400

    Update fortune.js
    trying something

--- fortune-js/fortune.js
+++ fortune-js/fortune.js
@@ -1,4 +1,13 @@
 quotepath = "friends.json";
 elementid = "fortune";
+
+
 $.getJSON(quotepath, function(test) {
-    document.getElementById(elementid).innerHTML = test[Math.floor(Math.random() * test.length)];});
+    if(TypeOf test === 'array'){
+        document.getElementById(elementid).innerHTML = test[Math.floor(Math.random() * test.length)];
+    }
+    else{
+        test2 = '["' + test.replace ('"', "'").replace("\n%\n", '"],["') + '"]';
+        document.getElementById(elementid).innerHTML = test2[Math.floor(Math.random() * test2.length)];
+    }
+    });
