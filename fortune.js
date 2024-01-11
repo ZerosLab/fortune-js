71aa9595c1c9b429545c2c7b32e10cd28867836a
Willow-Zero <zmckinnis4@gmail.com>
2024-01-10 20:55:43 -0500

    Update fortune.js

--- fortune.js
+++ fortune.js
@@ -1,11 +1,5 @@
 quotepath = "quotes.json";
 elementid = "fortune";
 element = document.getElementById(elementid);
-
-$.getJSON(quotepath, function(test) {
-    if(Array.isArray(test)){
-        element.innerHTML = test[Math.floor(Math.random() * test.length)].replace("\n","<br>");}
-//    else{
-//        test2 = JSON.parse('["' + test.replace ('"', "'").replace("\n%\n", '"],["') + '"]').replace("		"," ";
-//        element.innerHTML = test2[Math.floor(Math.random() * test2.length)];}}
-});
+  $.getJSON(quotepath, function(test) {
+    element.innerHTML = test[Math.floor(Math.random() * test.length)].replace("\n","<br>");});
