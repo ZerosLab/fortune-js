bcef60e6ac61568074b9fed2b10674cc9d54ff5d
Willow-Zero <zmckinnis4@gmail.com>
2024-01-10 20:18:42 -0500

    Update fortune.js

--- fortune.js
+++ fortune.js
@@ -4,8 +4,8 @@ element = document.getElementById(elementid);
 
 $.getJSON(quotepath, function(test) {
     if(Array.isArray(test)){
-        element.textContent = test[Math.floor(Math.random() * test.length)].replace("\n","<br>");}
+        element.innerHTML = test[Math.floor(Math.random() * test.length)].replace("\n","<br>");}
 //    else{
 //        test2 = JSON.parse('["' + test.replace ('"', "'").replace("\n%\n", '"],["') + '"]').replace("		"," ";
-//        element.textContent = test2[Math.floor(Math.random() * test2.length)];}}
+//        element.innerHTML = test2[Math.floor(Math.random() * test2.length)];}}
 });
