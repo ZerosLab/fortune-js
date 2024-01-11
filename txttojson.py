83f4abafd18d705eff2857bb57664e29c37efa12
Willow-Zero <zmckinnis4@gmail.com>
2024-01-10 20:57:11 -0500

    Update txttojson.py

--- txttojson.py
+++ txttojson.py
@@ -1,12 +1,10 @@
 import json
-
-u = input("File to convert:")
-
+u = input("File to convert: ")
 f = open(u, "r")
-dat = (f.read()) 
 
+dat = (f.read())
 datg = '["' + dat.replace ('http://', "").replace ('"', "") + '"]'
-final = datg.replace ("\n%\n", '", "').replace("\n"," ").replace("		"," ")
+final = datg.replace ("\n%\n", '", "').replace("\n"," ").replace("	"," ")
 
 f = open(u + ".json", "w")
 f.write(final)
