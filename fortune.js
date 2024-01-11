c281ce4f09fb60d8205a60c17d1e8dc9b80715da
Willow-Zero <zmckinnis4@gmail.com>
2024-01-10 19:42:24 -0500

    Update fortune.js

--- fortune.js
+++ fortune.js
@@ -1,10 +1,10 @@
 quotepath = "quotes.json";
 elementid = "fortune";
-element = document.getElementById(elementid).innerHTML;
+element = document.getElementById(elementid);
 
 $.getJSON(quotepath, function(test) {
     if(Array.isArray(test)){
-        element = test[Math.floor(Math.random() * test.length)];}
+        element.textContent = test[Math.floor(Math.random() * test.length)];}
     else{
         test2 = JSON.parse('["' + test.replace ('"', "'").replace("\n%\n", '"],["') + '"]');
-        element = test2[Math.floor(Math.random() * test2.length)];}});
+        element.textContent = test2[Math.floor(Math.random() * test2.length)];}});
