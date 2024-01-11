81823055348fc95751f260302c46ba6487ce8d18
Willow-Zero <zmckinnis4@gmail.com>
2024-01-10 20:09:36 -0500

    Update fortune.js

--- fortune.js
+++ fortune.js
@@ -4,9 +4,9 @@ element = document.getElementById(elementid);
 
 $.getJSON(quotepath, function(test) {
     console.log(test);
-//    if(Array.isArray(test)){
+    if(Array.isArray(test)){
         element.textContent = test[Math.floor(Math.random() * test.length)];}
 //    else{
-//        test2 = JSON.parse('["' + test.replace ('"', "'").replace("\n%\n", '"],["') + '"]');
+//        test2 = JSON.parse('["' + test.replace ('"', "'").replace("\n%\n", '"],["') + '"]').replace("		"," ";
 //        element.textContent = test2[Math.floor(Math.random() * test2.length)];}}
          );
