4ec5ea2e396de1fe5949ca7f4b35ba09c760c6d0
Willow-Zero <zmckinnis4@gmail.com>
2024-01-10 19:32:41 -0500

    Update fortune.js

--- fortune.js
+++ fortune.js
@@ -3,7 +3,7 @@ elementid = "fortune";
 element = document.getElementById(elementid).innerHTML
 
 $.getJSON(quotepath, function(test) {
-    if(TypeOf test === 'array'){
+    if((TypeOf test) === 'array'){
         element = test[Math.floor(Math.random() * test.length)];}
     else{
         test2 = '["' + test.replace ('"', "'").replace("\n%\n", '"],["') + '"]';
